f1811f77021039b6815f8443117992dda60ecff7
Kyojuronin <zump1379@gmail.com>
2022-04-26 22:20:53 +0700

    Create Exercise4_Putthipong_C.py

---
+++ Exercise4_Putthipong_C.py
@@ -0,0 +1,9 @@
+FE = "100"
+GB = "100"
+ICS = "100"
+CP = "100"
+print("--- Your Score ---")
+print("Foundation English" , ":" , (FE))
+print("General Business" , ":" , (GB))
+print("Introduction to Computer System" , ":" , (ICS))
+print("Computer Programming" , ":" , (CP))
