e8cf5828b204f40b4d355d62c28d1d8d5d55b72e
Kyojuronin <zump1379@gmail.com>
2022-04-27 22:41:29 +0700

    Create Exercise5_Putthipong_C.py
    Exercise_5_topic_1&2

---
+++ Exercise5_Putthipong_C.py
@@ -0,0 +1,19 @@
+print("----- Start Test 1 -----")
+a = int(input("1st:"))
+b = int(input("2nd:"))
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+print(c)
+print(d)
+print(e)
+print(f)
+print("----- End Test 1 -----")
+
+print("----- Start Test 2 -----")
+print("What's your Velocity?")
+s = float(input("Distance ?"))
+t = float(input("Time ?"))
+print("Velocity", s/t, "km/s")
+print("----- End Test 2 -----")
