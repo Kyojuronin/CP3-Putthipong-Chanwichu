469d64734be831c5d89f762683a6a0e569f9a975
Kyojuronin <zump1379@gmail.com>
2022-05-02 21:40:03 +0700

    Create Exercise12_createFunction.py

---
+++ Exercise12_createFunction.py
@@ -0,0 +1,12 @@
+def plusNumber(a,b):
+    print(a+b)
+def minusNumber(a,b):
+    print(a-b)
+def multipyNumber(a,b):
+    print(a*b)
+def divideNumber(a,b):
+    print(a/b)
+plusNumber(5,20)
+minusNumber(20,100)
+multipyNumber(5,50)
+divideNumber(100,2)
